3499c3714117829619a40c4e6acff48451d322cb
George Sandoval <gssandoval09@gmail.com>
2021-11-30 20:08:07 -0600

    fixed generateintern function to work properly

--- generateteam.js
+++ generateteam.js
@@ -37,7 +37,21 @@ const generateteam = team => {
             `
     }
     const generateintern = intern => {
-        
+        return `
+        <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:{{ email }}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">${intern.getSchool()}</li>
+        </ul>
+    </div>
+</div>
+        `
     }
     const html = []
     html.push(team.filter(employee => employee.getRole() === "Manager").map(manager => generatemanager(manager)))
